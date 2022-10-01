824d9732ae9fce05d1d87f6a675600113c8fd6de
HoJeong Go <seia@outlook.kr>
2022-10-01 20:40:20 +0900

    chore: use sql formatter

--- packages/watchtower/src/models/database/migrates/0001-initial-migration.sql
+++ packages/watchtower/src/models/database/migrates/0001-initial-migration.sql
@@ -1,39 +1,34 @@
 CREATE TABLE "user" (
-	i										SERIAL 	PRIMARY KEY NOT NULL,
-	email								TEXT			       		NOT NULL,
-	email_token					INT									NOT NULL,
-	password						TEXT								NOT NULL,
-	created_at					TIMESTAMP						NOT NULL,
-
+	i SERIAL PRIMARY KEY NOT NULL,
+	email TEXT NOT NULL,
+	email_token INT NOT NULL,
+	password TEXT NOT NULL,
+	created_at TIMESTAMP NOT NULL,
 	UNIQUE(email),
 	UNIQUE(i)
 );
 
 CREATE TABLE "instance" (
-	i										SERIAL 	PRIMARY KEY NOT NULL,
-	i_user							SERIAL 							NOT NULL,
-	status							INT									NOT NULL,
-	alias								TEXT								NOT NULL,
-	upstream						TEXT								NOT NULL,
-	created_at					TIMESTAMP						NOT NULL,
-	updated_at					TIMESTAMP						NOT NULL,
-
+	i SERIAL PRIMARY KEY NOT NULL,
+	i_user SERIAL NOT NULL,
+	status INT NOT NULL,
+	alias TEXT NOT NULL,
+	upstream TEXT NOT NULL,
+	created_at TIMESTAMP NOT NULL,
+	updated_at TIMESTAMP NOT NULL,
 	UNIQUE(i),
-
 	CONSTRAINT for_user FOREIGN KEY(i_user) REFERENCES "user"(i)
 );
 
 CREATE TABLE "blocklist" (
-	i										SERIAL 	PRIMARY KEY NOT NULL,
-	i_user							SERIAL							NOT NULL,
-	i_instance					SERIAL							NOT NULL,
-	name								TEXT								NOT NULL,
-	address							TEXT								NOT NULL,
-	created_at					TIMESTAMP						NOT NULL,
-	updated_at					TIMESTAMP						NOT NULL,
-
+	i SERIAL PRIMARY KEY NOT NULL,
+	i_user SERIAL NOT NULL,
+	i_instance SERIAL NOT NULL,
+	name TEXT NOT NULL,
+	address TEXT NOT NULL,
+	created_at TIMESTAMP NOT NULL,
+	updated_at TIMESTAMP NOT NULL,
 	UNIQUE(i),
-
 	CONSTRAINT for_user FOREIGN KEY(i_user) REFERENCES "user"(i),
 	CONSTRAINT for_instance FOREIGN KEY(i_instance) REFERENCES "instance"(i)
 );
