e4b373bbf41331fe08b6f05592e1c5d924b928c0
HoJeong Go <seia@outlook.kr>
2022-10-01 21:01:09 +0900

    chore: clean up

--- packages/watchtower/src/def.ts
+++
@@ -1,3 +0,0 @@
-import path from 'node:path';
-
-export const root = path.join(process.cwd(), 'aa-data');
---
+++ packages/watchtower/test/filter.test.ts
@@ -0,0 +1,26 @@
+import test from 'ava';
+import {writeFileSync} from 'fs';
+import {build} from '../src/models/filter.js';
+
+test('uAssets', async t => {
+	const uAssets = [
+		'https://ublockorigin.github.io/uAssets/filters/filters.txt',
+		'https://ublockorigin.github.io/uAssets/filters/badware.txt',
+		'https://ublockorigin.github.io/uAssets/filters/annoyances.txt',
+		'https://ublockorigin.github.io/uAssets/filters/quick-fixes.txt',
+		'https://ublockorigin.github.io/uAssets/filters/resource-abuse.txt',
+		'https://ublockorigin.github.io/uAssets/filters/unbreak.txt',
+		'https://ublockorigin.github.io/uAssets/filters/badlists.txt',
+		'https://ublockorigin.github.io/uAssets/filters/lan-block.txt',
+		'https://ublockorigin.github.io/uAssets/filters/annoyances.txt',
+	];
+
+	const sets = await build(uAssets);
+	const result = JSON.stringify(sets, null, 2);
+
+	writeFileSync('out.json', result);
+
+	t.truthy(sets.negative.find('html-online.com').found);
+	t.log(sets.positive.flatten().length, 'entries added to positives');
+	t.log(sets.negative.flatten().length, 'entries added to negatives');
+});
